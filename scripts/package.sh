51ffc95e48ac7389f340b236b2ecd9995df62b69
UnblockHub <unblockhub@protonmail.com>
2022-03-02 15:48:52 +0100

    prepare for deployment

--- scripts/package.sh
+++ scripts/package.sh
@@ -9,22 +9,8 @@ else
 fi
 
 yarn clean
+
+# Workaround for build issues, see #16555
+(cd node_modules/matrix-react-sdk; yarn reskindex)
+
 VERSION=$version yarn build
-
-# include the sample config in the tarball. Arguably this should be done by
-# `yarn build`, but it's just too painful.
-cp config.sample.json webapp/
-
-mkdir -p dist
-cp -r webapp element-$version
-
-# Just in case you have a local config, remove it before packaging
-rm element-$version/config.json || true
-
-$(dirname $0)/normalize-version.sh ${version} > element-$version/version
-
-tar chvzf dist/element-$version.tar.gz element-$version
-rm -r element-$version
-
-echo
-echo "Packaged dist/element-$version.tar.gz"
